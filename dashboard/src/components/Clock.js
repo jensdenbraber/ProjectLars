1ac78d9fceb3d8248b956f89777a73893473a679
Jens den Braber <jens.denbraber@gmail.com>
2021-10-26 21:54:24 +0200

    Add Clock to dashboard Fixes #16

---
+++ dashboard/src/components/Clock.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import moment from 'moment'
+
+import FloatingBox from './FloatingBox';
+
+const Clock = () => {
+
+    return (
+        <FloatingBox top="5%" left="80%" width="13%" height="15%" buttonChildren={<>{moment(new Date()).format("D MMMM HH:mm")}</>}>
+        </FloatingBox>
+    )
+}
+
+export default Clock;
