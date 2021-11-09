3332a0002955fabe6f9759a3441b312ae6f0f692
Jens den Braber <jens.denbraber@gmail.com>
2021-11-09 21:42:32 +0100

    Add night mode Fixes #34

--- src/components/HeadsUpDisplay.js
+++ src/components/HeadsUpDisplay.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Home, PowerSettingsNew } from '@mui/icons-material';
+import { Home } from '@mui/icons-material';
 
 import WaterTankLevels from './WaterTankLevels/waterTankLevels';
 import PowerLevels from './Power/PowerLevels';
 import Boiler from './Boiler/Boiler'
 import Gas from './Gas/GasLevels'
 import Clock from './Clock'
+import NighLight from './NighLight'
 
 import FloatingBox from './FloatingBox';
 
@@ -28,9 +29,8 @@ const HeadsUpDisplay = (props) => {
             </Gas>
             <Boiler top="60%" left="65%" width="13%" height="30%" {...props}>
             </Boiler>
-            <FloatingBox top="60%" left="80%" width="13%" height="30%" {...props} icon={<PowerSettingsNew className="full-screen" />}>
-                Power on/off
-            </FloatingBox>
+            <NighLight top="60%" left="80%" width="13%" height="30%" {...props}>
+            </NighLight>
             <Clock top="5%" left="80%" width="13%" height="15%">
             </Clock>
         </>
